17447b7532f633bb507325ebac709377cd467f05
vlg7t <locnv@vnpt.vn>
2023-03-06 17:04:16 +0700

    khocs

--- test-app/src/app/mitre/mitre.component.ts
+++ test-app/src/app/mitre/mitre.component.ts
@@ -7,7 +7,7 @@ import { ServerHttpService } from '../server-http.service';
   styleUrls: ['./mitre.component.scss']
 })
 export class MitreComponent implements OnInit {
-  mitre_tatic: string[] = [];
+  mitre_tatic: { id: number, name: string }[]= [];
   mitre_technique: string[] = [];
   drive_compromise: string[] = [];
 
@@ -16,14 +16,14 @@ export class MitreComponent implements OnInit {
     this.svHttp.getMitreTatics().subscribe((data) => {
       for (var n in data) {
         // console.log(data[n].technique_id);
-        this.mitre_tatic.push(data[n].name);
+        this.mitre_tatic.push({"id": data[n].id, "name": data[n].name});
         // tech_ids.push(data[n].technique_id);
       }
       // return tech_ids
     })
   }
 
-  get_mitre_technique() {
+  get_mitre_technique(tatic_id: number) {
     this.svHttp.getMitreTechnique().subscribe((data) => {
       // this.mitre_technique = data;
       for (var n in data) {
@@ -33,9 +33,11 @@ export class MitreComponent implements OnInit {
       }
     })
 
+    return tatic_id;
+
   }
 
-  get_drive_compromise() {
+  get_rule_content(mitre_tech_id: number) {
     this.svHttp.getDriveCompromise().subscribe((data) => {
       // this.drive_compromise = data;
       for (var n in data) {
@@ -44,14 +46,15 @@ export class MitreComponent implements OnInit {
         // tech_ids.push(data[n].technique_id);
       }
     })
+
   }
 
   constructor(private svHttp: ServerHttpService) { }
 
   ngOnInit(): void {
     this.get_mitre_tatics();
-    this.get_mitre_technique();
-    this.get_drive_compromise();
+    this.get_mitre_technique(1);
+    this.get_rule_content(1);
   }
 
 }
